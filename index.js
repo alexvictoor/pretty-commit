06a7edd52b2422fbce7f354887ea2adc246c99b4
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-08-18 22:27:17 +0200

    feat: cli help message

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-// @flow
-const nodegit = require('nodegit');
-const path = require('path');
+#!/usr/bin/env node
 
 const retrieveCommits = require('./retrieve-commits');
 const classifyCommits = require('./classify-commits');
 
 
-const [repoPath, lastCommitHash] = process.argv.slice(2);
+const [command, repoPath, lastCommitHash] = process.argv.slice(2);
 
+if (command === 'changelog') {
+    (async () => {
+        const commits = await retrieveCommits(repoPath, lastCommitHash);
+        const classifiedCommits = classifyCommits(commits);
+        console.log(JSON.stringify(classifiedCommits));
+    
+    })();
+} else {
+    console.log('-- Pretty commit ---------------------------------------------------------------------------');
+    console.log('-                                                                                          -');
+    console.log('-  Usage:    npx  pretty-commit  changelog  REPO_PATH  LAST_COMMIT                         -');
+    console.log('-                                                                                          -');
+    console.log('--------------------------------------------------------------------------------------------');
+}
 
-(async () => {
-    const commits = await retrieveCommits(repoPath, lastCommitHash);
-    const classifiedCommits = classifyCommits(commits);
-    console.log(JSON.stringify(classifiedCommits));
-
-})();
